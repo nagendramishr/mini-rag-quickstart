c91488efedc6c54c7cc6e27b447d96d8736829d7
Nagendra Mishr <nmishr@microsoft.com>
2024-04-10 19:28:28 -0400

    changes

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,9 +3,10 @@
 export RG=aoai-rag
 export COSMOS_ACCT=nvmaoaidb
 export COSMOS_DB=aoaidb
+export AOAI_APP=nvmaoai-teams
 
 export COSMOS_KEY=`az cosmosdb keys list --name $COSMOS_ACCT --resource-group $RG --type keys | jq -r '.primaryMasterKey'`
-
+export COSMOS_CONNSTR="AccountEndpoint=https://$COSMOS_ACCT.documents.azure.com:443/;AccountKey=$COSMOS_KEY;"
 # install the cosmos module
 pip install azure-cosmos
 
