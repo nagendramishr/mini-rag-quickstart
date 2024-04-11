fffa9211f6f09bc77b5301b033fd10e987c19592
Nagendra Mishr <nmishr@microsoft.com>
2024-04-10 20:05:27 -0400

    made the AOAI parameters configurable

--- src/azureFunction/function_app.py
+++ src/azureFunction/function_app.py
@@ -16,6 +16,12 @@ app = func.FunctionApp()
 def test_function(inputDocuments: func.DocumentList, req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    if not req.params.get('question'):
+        return func.HttpResponse(
+            "Please pass a question on the query string",
+            status_code=400
+        )
+
     if inputDocuments:
         client = AzureOpenAI(
             azure_endpoint = os.getenv("AOAI_ENDPOINT"), 
@@ -27,21 +33,21 @@ def test_function(inputDocuments: func.DocumentList, req: func.HttpRequest) -> f
         facts = "\n".join([doc.data['fact'] for doc in inputDocuments])                        
            
         message_text = [{"role":"system","content": facts}, 
-                        {"role":"user","content": "Who is most qualified to work on functions"}]
+                        {"role":"user","content": req.params.get('question')}]
 
         completion = client.chat.completions.create(
-            model="gpt35",
             messages = message_text,
-            temperature=0.7,
-            max_tokens=800,
-            top_p=0.95,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=None
+            model = os.environ.get("MODEL", "gpt35"),
+            temperature = float(os.environ.get("TEMPERATURE", "0.7")), 
+            max_tokens = int(os.environ.get("MAX_TOKENS", "800")),
+            top_p = float(os.environ.get("TOP_P", "0.95")),
+            frequency_penalty = float(os.environ.get("FREQUENCY_PENALTY", "0")),
+            presence_penalty = float(os.environ.get("PRESENCE_PENALTY", "0")),
+            stop = os.environ.get("STOP", "None")
         )
 
     return func.HttpResponse(
-        completion.choices[0].message.content,
+        "{\"response\": \""+completion.choices[0].message.content+"\"}",
         status_code=200
         )
 
